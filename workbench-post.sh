206e0dfca70e0adf367f273fbe7cb2829ee77d49
johnvpetersen <johnvpetersen@gmail.com>
2023-11-16 17:52:17 -0500

    Update workbench-post.sh

--- workbench-post.sh
+++ workbench-post.sh
@@ -1,13 +1,5 @@
 echo "Workbench: Post-processing begin"
 echo "*******************************"
 
-git submodule add https://github.com/johnvpetersen/DynamicLambda.git dependencies/submodules/dynamiclambda
-
-dotnet sln add dependencies/submodules/dynamiclambda/Core/Core.csproj
-dotnet sln add dependencies/submodules/dynamiclambda/Core.Test/Core.Test.csproj
-
-dotnet add tests/unit/unit.csproj reference dependencies/submodules/dynamiclambda/Core/Core.csproj
-dotnet add tests/integration/integration.csproj reference dependencies/submodules/dynamiclambda/Core/Core.csproj
-
 echo "*******************************"
 echo "Workbench: Post-processing end"
