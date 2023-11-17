b1edff542b1edc8793b0b75dd7535345199a7236
johnvpetersen <johnvpetersen@gmail.com>
2023-11-17 06:27:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,10 @@
+# clone the workbench repo to the specified directory. 
 git clone https://github.com/johnvpetersen/workbench.git <<target directory>>
+# remove the git repo since another one will be created.
 rm -rf <<target directory>>/workbench/.git
+# set the target directory as the working directory
 cd <<target directory>>
+# invoke the workbench script
 sh workbench/workbench.sh
+# launch vs code
 code .
